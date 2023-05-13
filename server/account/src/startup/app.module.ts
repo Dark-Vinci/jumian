6d9e9ae9c6055594c96e292d52df188f2535a6e8
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-05-13 19:02:02 +0100

    update concluded

--- server/account/src/startup/app.module.ts
+++ server/account/src/startup/app.module.ts
@@ -14,6 +14,7 @@ import User from '../models/user.model';
       isGlobal: true,
       envFilePath: ENV_FILE_PATH,
     }),
+
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -21,20 +22,24 @@ import User from '../models/user.model';
         configService: ConfigService,
       ): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> => {
         return {
-          port: +configService.get('DB_PORT'),
-          host: configService.get('DB_HOST'),
-          type: DbType[configService.get('DB_DBMS_TYPE')],
-          synchronize: Boolean(configService.get('DB_SYNCHRONY')),
-          entities: [User],
-          database: String(configService.get('DB_NAME')),
-          username: configService.get('DB_USERNAME'),
-          password: String(configService.get('DB_PASSWORD')),
+          port: configService.get<number>('DB_PORT'),
+          host: configService.get<string>('DB_HOST'),
+          type: DbType[configService.get<string>('DB_DBMS_TYPE')],
+          synchronize: configService.get<boolean>('DB_SYNCHRONY'),
+          database: configService.get<string>('DB_NAME'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
           logger: configService.get('DB_LOGGER_TYPE'),
-          migrationsTableName: configService.get('DB_MIGRATION_TABLE_NAME'),
-          logging: true,
+          migrationsTableName: configService.get<string>(
+            'DB_MIGRATION_TABLE_NAME',
+          ),
+          logging: configService.get('DB_LOGGING'),
+          entities: [User],
         };
       },
     }),
+
+    //     connect to redis
   ],
   controllers: [AppController],
   providers: [AppService],
